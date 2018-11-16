fa48c398c5dcfb0fb226764ae3f65d35be615632
zc_unoxx <zc_work@outlook.com>
2018-11-16 21:38:29 +0800

    SSM校园商铺1.0

--- src/main/webapp/resources/js/shop/productcategorymanagement.js
+++ src/main/webapp/resources/js/shop/productcategorymanagement.js
@@ -3,7 +3,7 @@ $(function () {
     //后台从session中获取shop的信息，这里不传shopId
     var listUrl = '/o2o/shopadmin/getproductcategorylist';
     var addUrl = '/o2o/shopadmin/addproductcategorys';
-    var deleteUrl = '/o2o/shopadmin/removepeoductcategory';
+    var deleteUrl = '/o2o/shopadmin/removeproductcategory';
 
     // 调用getProductCategoryList，加载数据
     getProductCategoryList();
@@ -23,7 +23,7 @@ $(function () {
                         + '<div class="col-33">'
                         + item.priority
                         + '</div>'
-                        + '<div class="col-33"><a href="#" class="button delete" '
+                        + '<div class="col-33"><a href="#" class="button delete" data-id=" '
                         + item.productCategoryId
                         + '">删除</a></div>'
                         + '</div>';
@@ -32,4 +32,81 @@ $(function () {
             }
         });
     }
-})
+
+
+    //定义新增按钮事件  每点击一次都会出现一个新的div
+    $('#new').click(function () {
+        var tempHtml = '<div class="row row-product-category temp">'
+            + '<div class="col-33"><input class="category-input category" type="text" placeholder="分类名"></div>'
+            + '<div class="col-33"><input class="category-input priority" type="number" placeholder="优先级"></div>'
+            + '<div class="col-33"><a href="#" class="button delete">删除</a></div>'
+            + '</div>';
+
+        $('.product-category-wrap').append(tempHtml);
+    });
+
+    $('#submit').click(function () {
+        //通过temp获取新增的行
+        var tempArr = $('.temp');
+        // 定义数组接收新增的数据
+        var productCategoryList = [];
+        tempArr.map(function (index,item){
+            var tempObj = {};
+            tempObj.productCategoryName = $(item).find('.category').val();
+            tempObj.priority = $(item).find('.priority').val();
+            if(tempObj.productCategoryName && tempObj.priority){
+                productCategoryList.push(tempObj);
+            }
+        });
+
+        //Ajax
+        $.ajax({
+            url:addUrl,
+            type:'POST',
+            // 后端通过 @HttpRequestBody直接接收
+            data:JSON.stringify(productCategoryList),
+            contentType:'application/json',
+            success:function (data) {
+                if(data.success){
+                    $.toast('新增信息成功');
+                    //重新加载数据
+                    getProductCategoryList();
+                }else{
+                    $.toast('提交失败--'+data.errMsg);
+                }
+            }
+        });
+    });
+
+
+    // 一种是需要提交到后台的删除now  ，另外一种是新增但未提交到数据库中的删除temp
+    //temp
+    $('.product-category-wrap').on('click','.row-product-category.temp .delete',function (e) {
+        /*console.log($(this).parent().parent());*/
+        $(this).parent().parent(),remove();
+    })
+
+    //now
+    $('.product-category-wrap').on('click','.row-product-category.now .delete',function (e) {
+        var target = e.currentTarget;
+        $.confirm("确定么?",function () {
+            $.ajax({
+                url:deleteUrl,
+                type:'POST',
+                data:{
+                    productCategoryId : target.dataset.id,
+                },
+                dataType:'json',
+                success:function (data) {
+                    if(data.success){
+                        $.toast('删除成功(ノω<。)ノ))');
+                        //重新加载数据
+                        getProductCategoryList();
+                    }else{
+                        $.toast('删除失败(┬＿┬)');
+                    }
+                }
+            });
+        });
+    });
+});
