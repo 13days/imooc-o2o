ef115e2bb9b34a4d12bd4fa9c41ecccd02674b67
zc_unoxx <zc_work@outlook.com>
2018-12-05 11:24:03 +0800

    商品添加

--- src/main/java/com/imooc/o2o/dao/ProductImgDao.java
+++ src/main/java/com/imooc/o2o/dao/ProductImgDao.java
@@ -20,6 +20,11 @@ public interface ProductImgDao {
      */
     int batchInsertProductImg(List<ProductImg> productImgList);
 
+    /**
+     * 删除指定商品下的所有详情图
+     * @param productId
+     * @return
+     */
     int deleteProductImgByProductId(long productId);
 
 }
