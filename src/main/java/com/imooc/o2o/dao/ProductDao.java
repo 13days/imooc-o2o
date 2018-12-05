5f2c52df561d2ca5273c8a2570733ce4aa3d9b3f
zc_unoxx <zc_work@outlook.com>
2018-12-05 11:23:34 +0800

    商品添加

--- src/main/java/com/imooc/o2o/dao/ProductDao.java
+++ src/main/java/com/imooc/o2o/dao/ProductDao.java
@@ -1,6 +1,9 @@
 package com.imooc.o2o.dao;
 
 import com.imooc.o2o.entity.Product;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * @Author: Alex
@@ -8,10 +11,41 @@ import com.imooc.o2o.entity.Product;
  * @Annotation:
  */
 public interface ProductDao {
+
+    /**
+     * 更新商品信息
+     * @param product
+     * @return
+     */
+    int updateProduct(Product product);
+
+    /**
+     * 通过productId查询唯一的商品信息
+     * @param productId
+     * @return
+     */
+    Product queryProductById(long productId);
+
     /**
      * 插入商品
      * @param product
      * @return
      */
     int insertProduct(Product product);
+
+    /**
+     * 查询商品列表并分页，可输入的条件有：商品名（模糊），商品状态，店铺类别，商品类别
+     * @param productCondition
+     * @param rowIndex
+     * @param pageSize
+     * @return
+     */
+    List<Product> queryProductList(@Param("productCondition")Product productCondition,@Param("rowIndex")int rowIndex,@Param("pageSize")int pageSize);
+
+    /**
+     * 查询对应商品总数
+     * @param productCondition
+     * @return
+     */
+    int queryProductCount(@Param("productCondition")Product productCondition);
 }
